27c1f8f7bf527730c368d177597c3c24d79e8c93
Gavin Quirk <gavinquirk@gmail.com>
2021-10-11 07:44:33 -0700

    lander

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -9,7 +9,12 @@ export const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: bottom: 40px;
+  margin-bottom: 40px;
 `;
 
 export const Logo = styled.img``;
+
+export const Image = styled.img`
+  width: 375px;
+  margin-left: 40px;
+`;
